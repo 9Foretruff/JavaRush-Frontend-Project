d7b1be38706504322472417e6679d2c0037f6dc2
Rokitko Maksim <rokitkomaksim@gmail.com>
2024-01-27 23:32:32 +0200

    fix 2

--- src/main/webapp/html/script.js
+++ src/main/webapp/html/script.js
@@ -17,15 +17,15 @@ function fillTable(pageNumber, pageSize) {
 
         players.forEach((player) => {
             htmlRows +=
-                `<tr>
-                    <td class="cell"> ${player.id}</td>
-                    <td class="cell"> ${player.name}</td>
-                    <td class="cell"> ${player.title}</td>
-                    <td class="cell"> ${player.race}</td>
-                    <td class="cell"> ${player.profession}</td>
-                    <td class="cell"> ${player.level}</td>
-                    <td class="cell"> ${player.birthday}</td>
-                    <td class="cell"> ${player.banned}</td>
+                `<tr class="row" data-account-id="${player.id}">
+                    <td class="cell cell_small"> ${player.id}</td>
+                    <td class="cell" data-account-name> ${player.name}</td>
+                    <td class="cell" data-account-title> ${player.title}</td>
+                    <td class="cell" data-account-race> ${player.race}</td>
+                    <td class="cell" data-account-profession> ${player.profession}</td>
+                    <td class="cell" data-account-level> ${player.level}</td>
+                    <td class="cell" data-account-birthday> ${new Date(player.birthday).toLocaleDateString()}</td>
+                    <td class="cell" data-account-banned> ${player.banned}</td>
                     <td class="cell cell-auto"> 
                         <button class="edit-button" value="${player.id}">
                             <img class="edit-image" src="../img/edit.png" alt="edit">
@@ -45,10 +45,10 @@ function fillTable(pageNumber, pageSize) {
         $playersTableBody.insertAdjacentHTML('beforeend', htmlRows)
 
         const deleteButtons = document.querySelectorAll('.delete-button');
-        deleteButtons.forEach(button => button.addEventListener('click',removeAccountHandler))
+        deleteButtons.forEach(button => button.addEventListener('click', removeAccountHandler))
 
         const editButtons = document.querySelectorAll('.edit-button');
-        editButtons.forEach(button => button.addEventListener('click',editAccountHandler))
+        editButtons.forEach(button => button.addEventListener('click', editAccountHandler))
     })
 
 }
@@ -68,7 +68,7 @@ function updatePaginationButtons() {
     let paginationButtonsHtml = '';
 
     for (let i = 1; i <= accountsAmount; i++) {
-        paginationButtonsHtml += '<button value="' + (i - 1) + '">' + i  + '</button>'
+        paginationButtonsHtml += '<button value="' + (i - 1) + '">' + i + '</button>'
     }
 
     if (childButtonsCount !== 0) {
@@ -110,7 +110,7 @@ function onPageChange(e) {
     setActiveButton(currentPageNumber);
 }
 
-function setActiveButton(buttonIndex = 0){
+function setActiveButton(buttonIndex = 0) {
     const $buttonsContainer = document.querySelector('.pagination-buttons');
     const $targetButton = Array.from($buttonsContainer.children)[buttonIndex];
     const $currentActiveButton = Array.from($buttonsContainer.children)[currentPageNumber];
@@ -119,19 +119,53 @@ function setActiveButton(buttonIndex = 0){
     $targetButton.classList.add('active-pagination-button')
 }
 
-function removeAccountHandler(e){
+function removeAccountHandler(e) {
     const accountId = e.currentTarget.value;
 
     $.ajax({
         url: `/rest/players/${accountId}`,
         type: 'DELETE',
-        success: function (){
+        success: function () {
             updatePlayersCount()
-            fillTable(currentPageNumber,accountPerPage)
+            fillTable(currentPageNumber, accountPerPage)
         }
     })
 }
 
-function editAccountHandler(e){
+function editAccountHandler(e) {
     const accountId = e.currentTarget.value;
+
+    const $currentRow = document.querySelector(`.row[data-account-id='${accountId}']`);
+    const $currentRemoveButton = $currentRow.querySelector('.delete-button');
+    const $currentImage = $currentRow.querySelector('.edit-button img');
+
+    const $currentName = $currentRow.querySelector('[data-account-name]');
+    const $currentTitle = $currentRow.querySelector('[data-account-title]');
+    const $currentRace = $currentRow.querySelector('[data-account-race]');
+    const $currentProfession = $currentRow.querySelector('[data-account-profession]');
+    const $currentBanned = $currentRow.querySelector('[data-account-banned]');
+
+    $currentImage.src = "../img/save.png"
+
+    $currentName.childNodes[0].replaceWith(createInput($currentName.innerHTML))
+    $currentTitle.childNodes[0].replaceWith(createInput($currentTitle.innerHTML))
+    $currentRace.childNodes[0].replaceWith(createInput($currentRace.innerHTML))
+    $currentProfession.childNodes[0].replaceWith(createInput($currentProfession.innerHTML))
+    $currentBanned.childNodes[0].replaceWith(createInput($currentBanned.innerHTML))
+}
+
+
+function createInput(value) {
+    const $htmlInputElement = document.createElement('input');
+
+    $htmlInputElement.setAttribute('type', 'text')
+    $htmlInputElement.setAttribute('value', value)
+    $htmlInputElement.setAttribute('data-value', value)
+
+    $htmlInputElement.addEventListener('input', e => {
+        $htmlInputElement.setAttribute('data-value', `${e.currentTarget.value}`)
+    })
+
+    return $htmlInputElement
+
 }
